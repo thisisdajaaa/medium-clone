7d335514572aa464847112d643cf8f1074c9a3e8
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2022-05-01 00:53:06 +0800

    feat: initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next Apps</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
